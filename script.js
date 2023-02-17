738503919dc6424adb38320f9aa3be1290f7f420
Camila Gallegos <camigm1@gmail.com>
2023-02-17 12:51:53 -0700

    append containers

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let divContainer = document.querySelector(".container");
+let divContainer = document.getElementById("container");
 // console.log(divContainer);
-
-for (var i = 0; i <= 15; i++) {
+let boxes = new Array();
+boxes.length = 16;
+// let specialBox = boxes[3];
+// specialBox.textContent = "special";
+for (let i = 1; i <= 16; i++) {
   let div = document.createElement("div");
   div.className = "squares";
   div.textContent = "is this working";
-  console.log(div);
-  divContainer.appendNode(div);
+  divContainer.appendChild(div);
   console.log(divContainer);
 }
 
-// console.log("is this working");
+console.log(divContainer);
