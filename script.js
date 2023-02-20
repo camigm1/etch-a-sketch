4498302a490faa67a8158fa689a1eba4774f18b8
Camila Gallegos <camigm1@gmail.com>
2023-02-19 20:26:56 -0700

    added more

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 let divContainer = document.getElementById("container");
 let squares = document.querySelector(".squares");
+
 for (let i = 1; i <= 256; i++) {
   let div = document.createElement("div");
   div.className = "squares";
   div.style.backgroundColor = "#ff8989";
-
   divContainer.appendChild(div);
 }
-
-console.log(divContainer);
-squares.addEventListener("mouseover", function () {
-  div.style.backgroundColor = "#333";
-});
