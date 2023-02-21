282277339e396b69cf4389f0c187082be556afe7
Camila Gallegos <camigm1@gmail.com>
2023-02-21 14:06:13 -0700

    changing square color

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let divContainer = document.getElementById("container");
-let squares = document.querySelector(".squares");
 
 for (let i = 1; i <= 256; i++) {
   let div = document.createElement("div");
   div.className = "squares";
   div.style.backgroundColor = "#ff8989";
   divContainer.appendChild(div);
+  div.addEventListener("mouseenter", function (e) {
+    let squares = e.target;
+    squares.style.backgroundColor = "#333";
+  });
 }
